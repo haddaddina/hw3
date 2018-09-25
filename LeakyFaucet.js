1f4f181780260b5ad6f36f822b06a95b344f0e9d
haddaddina <43105313+haddaddina@users.noreply.github.com>
2018-09-24 22:59:00 -0700

    Update LeakyFaucet.js

--- LeakyFaucet.js
+++ LeakyFaucet.js
@@ -2,6 +2,9 @@ var x = 230;
 var y = 120;
 var puddle = 30
 var ize = 5
+var y0 = 120;
+var y1 = 180;
+var y2 = 220;
 
 
 function setup() {
@@ -15,25 +18,56 @@ function draw() {
   // draw pipe
   rect(0, 100, x, 20, 50);
 
-  // draw puddle
-  ellipse(230, 400 , puddle, ize);
-  
  	// draw drip
   ellipse(x, y, 10);
+  ellipse(x, y0, 10);
+  ellipse(x, y1, 10);
+  ellipse(x, y2, 10);
+  
+    // draw puddle
+  ellipse(230, 400 , puddle, ize);
   
   
   if (y > 380) {
   puddle = puddle + 0.05
    
   }
+  if (y0 > 380) {
+  puddle = puddle + 0.05
+   
+  }
+  if (y1 > 380) {
+  puddle = puddle + 0.05
+   
+  }
+  if (y2 > 380) {
+  puddle = puddle + 0.05
+   
+  }
   
   // down 3 pixels each frame, but maybe should be accelerating?
   y = y + 9
+  y0 = y0 + 9
+  y1 = y1 + 9
+  y2 = y2 + 9
   
   
   // if invisible for a full “height” amount…
   if (y > height*2) {
     // reset
     y = 120;
+   
+    // if invisible for a full “height” amount, reset
+  if (y0 > height*2) {
+     y0 = 120;
   }
+
+  if (y1 > height*2) {
+     y1 = 120;
+  }
+
+  if (y2 > height*2) {
+     y2 = 120;
+  }
+}
 }
